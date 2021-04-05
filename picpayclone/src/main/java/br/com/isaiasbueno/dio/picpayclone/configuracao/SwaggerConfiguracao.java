909e95ba840bfd4691f6a59e01649dfc517c52ba
IsaiasBueno <izaias.bueno@gmail.com>
2021-04-05 16:26:16 -0300

    Commit/Update 05/04/2021

--- picpayclone/src/main/java/br/com/isaiasbueno/dio/picpayclone/configuracao/SwaggerConfiguracao.java
+++ picpayclone/src/main/java/br/com/isaiasbueno/dio/picpayclone/configuracao/SwaggerConfiguracao.java
@@ -1,11 +1,8 @@
 package br.com.isaiasbueno.dio.picpayclone.configuracao;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +11,8 @@ import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -25,7 +24,7 @@ public class SwaggerConfiguracao {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.dio.picpayclone")).paths(PathSelectors.any()).build()
+                .apis(RequestHandlerSelectors.basePackage("br.com.isaiasbueno.dio.picpayclone")).paths(PathSelectors.any()).build()
                 .apiInfo(apiInfo()).useDefaultResponseMessages(false)
                 .securitySchemes(Arrays.asList(apiKey()));
 
