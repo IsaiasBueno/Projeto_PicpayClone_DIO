25365ff800c2edbbb513139186c93be4ab966e17
IsaiasBueno <izaias.bueno@gmail.com>
2021-04-06 09:49:41 -0300

    Commit/Update 06/04/2021

---
+++ picpayclone/src/main/java/br/com/isaiasbueno/dio/picpayclone/resource/AutenticacaoResource.java
@@ -0,0 +1,42 @@
+package br.com.isaiasbueno.dio.picpayclone.resource;
+
+import br.com.isaiasbueno.dio.picpayclone.dto.LoginDTO;
+import br.com.isaiasbueno.dio.picpayclone.dto.TokenDTO;
+import br.com.isaiasbueno.dio.picpayclone.service.impl.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/autenticacao")
+public class AutenticacaoResource extends ResourceBase<TokenDTO> {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<TokenDTO> autenticar(@RequestBody @Valid LoginDTO loginDTO) {
+        UsernamePasswordAuthenticationToken dadosLogin = loginDTO.converter();
+
+        try {
+            Authentication authentication = authManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(authentication);
+            return responderSucessoComItem(new TokenDTO(token, "Bearer"));
+        } catch (AuthenticationException e) {
+            return responderRequisicaoMalSucedida();
+        }
+    }
+
+}
---
+++ picpayclone/src/main/resources/data_.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuarios ( id, usu_login, usu_senha, usu_email, usu_nome_completo, usu_cpf, usu_data_nascimento, usu_numero_telefone, usu_saldo, usu_ativo, usu_permissao ) VALUES ( 1, 'isaiasbueno', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq', 'izaias.bueno@gmail.com', 'Isaias Candido Bueno', '111.111.111-11', '1987-06-14', '62992920466', 0, true, 'USUARIO');
+
+INSERT INTO usuarios ( id, usu_login, usu_senha, usu_email, usu_nome_completo, usu_cpf, usu_data_nascimento, usu_numero_telefone, usu_saldo, usu_ativo, usu_permissao ) VALUES ( 2, 'cicerojmm', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq', 'cicerojmm@gmail.com', 'Cicero Moura', '222.222.222-22', '1995-06-20', '62984253536', 0, true, 'USUARIO' );
+
+INSERT INTO usuarios ( id, usu_login, usu_senha, usu_email, usu_nome_completo, usu_cpf, usu_data_nascimento, usu_numero_telefone, usu_saldo, usu_ativo, usu_permissao ) VALUES ( 3, 'paulovitor', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq', 'paulovitor@gmail.com', 'Paulo Vitor', '333.333.333-33', '1970-06-20', '62984252326', 0, true, 'USUARIO' );
+
+INSERT INTO usuarios ( id, usu_login, usu_senha, usu_email, usu_nome_completo, usu_cpf, usu_data_nascimento, usu_numero_telefone, usu_saldo, usu_ativo, usu_permissao ) VALUES ( 4, 'pedrovf', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq', 'pedrovf@gmail.com', 'Pedro Freitas', '444.444.444-44', '1990-06-20', '62974656563', 0, true, 'USUARIO' );
